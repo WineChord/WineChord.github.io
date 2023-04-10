384ab1a4752df7f9ec6f110e316afb372d79b955
WineChord <guoqizhou123123@qq.com>
2023-04-10 00:43:12 -0400

    update

---
+++ coding/cf/cr280d2/B.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+/*
+https://codeforces.com/problemset/problem/492/B
+
+Codeforces Round 280 (Div. 2) B. Vanya and Lanterns 
+
+Vanya walks late at night along a straight street of length l, lit by n 
+lanterns. Consider the coordinate system with the beginning of the street 
+corresponding to the point 0, and its end corresponding to the point l. 
+Then the i-th lantern is at the point a_i. The lantern lights all points 
+of the street that are at the distance of at most d from it, where d is 
+some positive number, common for all lanterns. 
+
+Vanya wonders: what is the minimum light radius d should the lanterns have 
+to light the whole street?
+*/
+void run(){
+    // Welcome, your majesty.
+}
+int main(){
+#ifdef WINE
+    freopen("data.in","r",stdin);
+#endif
+    int T;scanf("%d",&T);
+    while(T--){
+        run();
+    }
+}
---
+++ coding/cf/cr820d3/C.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+/*
+https://codeforces.com/problemset/problem/1729/C
+
+Codeforces Round 820 (Div. 3) C. Jumping on Tiles 
+
+Polycarp was given a row of tiles. Each tile contains one lowercase letter 
+of the Latin alphabet. The entire sequence of tiles forms the string s.
+
+In other words, you are given a string s consisting of lowercase Latin 
+letters.
+
+Initially, Polycarp is on the first tile of the row and wants to get to 
+the last tile by jumping on the tiles. Jumping from i-th tile to j-th tile 
+has a cost equal to |index(s_i) - index(s_j)|, where index(c) is the index 
+of the letter c in the alphabet (for example, index('a')=1, index('b')=2, 
+..., index('z')=26) .
+
+Polycarp wants to get to the n-th tile for the minimum total cost, but at 
+the same time make maximum number of jumps.
+
+In other words, among all possible ways to get to the last tile for the 
+minimum total cost, he will choose the one with the maximum number of 
+jumps.
+
+Polycarp can visit each tile at most once.
+
+Polycarp asks you to help — print the sequence of indices of string s on 
+which he should jump.
+*/
+void run(){
+    // Welcome, your majesty.
+}
+int main(){
+#ifdef WINE
+    freopen("data.in","r",stdin);
+#endif
+    int T;scanf("%d",&T);
+    while(T--){
+        run();
+    }
+}
