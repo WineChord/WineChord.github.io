5d0453ab2793cce72f10eed744c8275d17e02783
wineandchord <guoqizhou19@gmail.com>
2023-10-23 04:12:59 +0800

    add atcoder

---
+++ coding/cf/ecr152rd2/B.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+/*
+https://codeforces.com/problemset/problem/1849/B
+
+Educational Codeforces Round 152 (Rated for Div. 2) B. Monsters 
+
+Monocarp is playing yet another computer game. And yet again, his 
+character is killing some monsters. There are n monsters, numbered from 1 
+to n, and the i-th of them has a_i health points initially.
+
+Monocarp's character has an ability that deals k damage to the monster 
+with the highest current health. If there are several of them, the one 
+with the smaller index is chosen. If a monster's health becomes less than 
+or equal to 0 after Monocarp uses his ability, then it dies.
+
+Monocarp uses his ability until all monsters die. Your task is to 
+determine the order in which monsters will die.
+*/
+void run(){
+    // Welcome, your majesty.
+}
+int main(){
+#ifdef WINE
+    freopen("data.in","r",stdin);
+#endif
+    int T;scanf("%d",&T);
+    while(T--){
+        run();
+    }
+}
